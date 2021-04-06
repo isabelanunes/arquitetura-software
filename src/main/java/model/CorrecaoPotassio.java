a32e7975f51471719ae6f530fe33166897ae7e0f
Isabela <isabela.caetano2@etec.sp.gov.br>
2021-04-05 21:37:02 -0300

    refatoração dos métodos valorFontePotassio e correcaoPotassioFornece - movidos para o enum FontePotassio

--- src/main/java/model/CorrecaoPotassio.java
+++ src/main/java/model/CorrecaoPotassio.java
@@ -40,19 +40,6 @@ public class CorrecaoPotassio {
             return 0.0;
     }
     
-    public double valorFontePotassio(FontesPotassio fontesPotassio){
-        switch(fontesPotassio){
-            case CLORETO_POTASSIO:
-                return 58.0;
-            case SULFATO_POTASSIO:
-                return 52.0;
-            case SULFATO_POTASSIO_MAGNESIO:
-                return 22.0;
-            default:
-                return 0.0;
-        }
-    }
-    
     public double quantidadeAplicarPotassio(){
         double necessidadeAdicionar = this.solo.getPotassio()*
                 this.participacaoPotassioDesejada/this.participacaoPotassioAtual()
@@ -61,22 +48,11 @@ public class CorrecaoPotassio {
             return 0.0;
         else
             return necessidadeAdicionar * 39.1 * 10 * 2 * 1.2 * 100 / 0.85 /100 *100 
-                    / this.valorFontePotassio(this.fontePotassio);
+                    / this.fontePotassio.valorFontePotassio();
     }
     
     public ItemCorrecaoFornece[] correcaoPotassioFornece(){
-        ItemCorrecaoFornece[] itemCorrecaoFornece = new ItemCorrecaoFornece[2];
-        switch (this.fontePotassio){
-            case SULFATO_POTASSIO:
-                itemCorrecaoFornece[0] = new ItemCorrecaoFornece(this.quantidadeAplicarPotassio()*0.17, Nutrientes.ENXOFRE);
-                return itemCorrecaoFornece;
-            case SULFATO_POTASSIO_MAGNESIO:
-                itemCorrecaoFornece[0] = new ItemCorrecaoFornece(this.quantidadeAplicarPotassio()*0.22, Nutrientes.ENXOFRE);
-                itemCorrecaoFornece[1] = new ItemCorrecaoFornece(this.quantidadeAplicarPotassio()*0.18, Nutrientes.MAGNESIO);
-                return itemCorrecaoFornece;
-            default:
-                return null;
-        }        
+        return this.fontePotassio.correcaoPotassioFornece(this);
     }
         
     public double custoAlqueirePotassio(){
--- src/main/java/model/FontesPotassio.java
+++ src/main/java/model/FontesPotassio.java
@@ -10,7 +10,39 @@ package model;
  * @author Isabela Nunes
  */
 public enum FontesPotassio {
-    CLORETO_POTASSIO,
-    SULFATO_POTASSIO,
-    SULFATO_POTASSIO_MAGNESIO
+    CLORETO_POTASSIO{
+        public double valorFontePotassio() {
+            return 58.0;
+        }
+        
+        public ItemCorrecaoFornece[] correcaoPotassioFornece(CorrecaoPotassio correcaoPotassio){
+            return null;
+        }
+    },
+    SULFATO_POTASSIO{
+        public double valorFontePotassio() {
+            return 52.0;
+        }
+        
+        public ItemCorrecaoFornece[] correcaoPotassioFornece(CorrecaoPotassio correcaoPotassio){
+            ItemCorrecaoFornece[] itemCorrecaoFornece = new ItemCorrecaoFornece[1];
+            itemCorrecaoFornece[0] = new ItemCorrecaoFornece(correcaoPotassio.quantidadeAplicarPotassio()*0.17, Nutrientes.ENXOFRE);
+            return itemCorrecaoFornece;
+        }
+    },
+    SULFATO_POTASSIO_MAGNESIO{
+        public double valorFontePotassio() {
+            return 22.0;
+        }
+        
+        public ItemCorrecaoFornece[] correcaoPotassioFornece(CorrecaoPotassio correcaoPotassio){
+            ItemCorrecaoFornece[] itemCorrecaoFornece = new ItemCorrecaoFornece[2];
+            itemCorrecaoFornece[0] = new ItemCorrecaoFornece(correcaoPotassio.quantidadeAplicarPotassio()*0.22, Nutrientes.ENXOFRE);
+            itemCorrecaoFornece[1] = new ItemCorrecaoFornece(correcaoPotassio.quantidadeAplicarPotassio()*0.18, Nutrientes.MAGNESIO);
+            return itemCorrecaoFornece;
+        }
+    };
+    
+    public abstract double valorFontePotassio();
+    public abstract ItemCorrecaoFornece[] correcaoPotassioFornece(CorrecaoPotassio correcaoPotassio);
 }
